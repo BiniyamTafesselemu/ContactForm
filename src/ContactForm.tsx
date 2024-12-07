5b77dd0967fb2da916aafa4ec1b79814f18cfacd
BiniyamTafesse <biniyamtafesse228@gmail.com>
2024-12-07 10:52:42 +0300

    extra on css

--- src/ContactForm.tsx
+++ src/ContactForm.tsx
@@ -30,7 +30,7 @@ const ContactForm = () => {
     })  
   });  
 
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({  
+  const { register, handleSubmit, formState: { errors }, setError } = useForm<FormData>({  
     resolver: zodResolver(schema)  
   });  
 
@@ -42,8 +42,13 @@ const ContactForm = () => {
     }, 3000);  // Show popup for 3 seconds  
   };  
 
+  const handleRadioChange = (value: string) => {
+    setSelectedQuery(value);
+    setError('query', { message: '' }); // Clear the error message for the query field
+  };
+
   return (  
-    <div>  
+    <div className="min-h-screen flex items-center justify-center bg-lightgreen">  
       {showPopup && (  
         <div className="fixed top-0 left-0 right-0 bg-darkgreen text-white p-4 flex flex-col items-center justify-center z-50">  
           <div className="flex flex-col bg-darkergray p-4 rounded-md">  
@@ -55,94 +60,86 @@ const ContactForm = () => {
           </div>  
         </div>  
       )}  
-      <form onSubmit={handleSubmit(onSubmit)} className="w-full bg-lightgreen flex items-center justify-center min-h-screen">  
-        <div className="flex flex-col bg-white w-full max-w-2xl p-4 rounded-2xl">  
-          <div className="p-[10px]">  
-            <h1 className="text-2xl font-bold font-Karla">Contact Us</h1>  
-            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mt-[20px]">  
-              <div className="flex flex-col flex-1">  
-                <label htmlFor="firstname">First Name<span className=" ml-[10px] text-green-700">*</span></label>  
-                <input  
-                  className="border cursor-pointer border-black h-[40px] rounded-[5px] p-1 mt-[10px] hover:border-2 hover:border-green-500"  
-                  id="firstname"  
-                  type="text"  
-                  {...register('firstname')}  
-                />  
-                {errors.firstname && <p className="text-red-600">{errors.firstname.message}</p>}  
-              </div>  
-              <div className="flex flex-col flex-1">  
-                <label htmlFor="lastname">Last Name<span className=" ml-[10px] text-green-700">*</span></label>  
-                <input  
-                  className="border cursor-pointer border-black h-[40px] rounded-[5px] p-1 mt-[10px] hover:border-2 hover:border-green-500"  
-                  id="lastname"  
-                  type="text"  
-                  {...register('lastname')}  
-                />  
-                {errors.lastname && <p className="text-red-600">{errors.lastname.message}</p>}  
-              </div>  
-            </div>  
-            <div className="flex flex-col mt-[20px]">  
-              <label htmlFor="email">Email<span className=" ml-[10px] text-green-700">*</span></label>  
-              <input  
-                type="email"  
-                id="email"  
-                className="p-1 border cursor-pointer border-black rounded-[5px] w-full mt-[10px] h-[40px] hover:border-2 hover:border-green-500"  
-                {...register('email')}  
-              />  
-              {errors.email && <p className="text-red-600">{errors.email.message}</p>}  
-            </div>  
-            <div className="flex flex-col mt-[20px]">  
-              <label>Query Type<span className=" ml-[10px] text-green-700">*</span></label>  
-              <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">  
-                <div  
-                  className={`flex items-center border p-2 rounded-md w-full md:w-[300px] mt-[10px] ${selectedQuery === 'GeneralEnquiry' ? 'bg-lightgreen' : 'bg-white'} focus-within:border-2 focus-within:border-green-500`}  
-                >  
-                  <input  
-                    type="radio"  
-                    id="generalenquiry"  
-                    value="GeneralEnquiry"  
-                    className="ml-[18px] mr-2 focus:ring-2 focus:ring-green-500"  
-                    {...register('query')}  
-                    onChange={() => setSelectedQuery('GeneralEnquiry')}  
-                  />  
-                  <label htmlFor="generalenquiry">General Enquiry</label>  
-                </div>  
-                <div  
-                  className={`flex items-center border p-2 rounded-md w-full md:w-[300px] mt-[10px] ${selectedQuery === 'SupportRequest' ? 'bg-lightgreen' : 'bg-white'} focus-within:border-2 focus-within:border-green-500`}  
-                >  
-                  <input  
-                    type="radio"  
-                    id="supportrequest"  
-                    value="SupportRequest"  
-                    className="ml-[18px] mr-2 focus:ring-2 focus:ring-green-500"  
-                    {...register('query')}  
-                    onChange={() => setSelectedQuery('SupportRequest')}  
-                  />  
-                  <label htmlFor="supportrequest">Support Request</label>  
-                </div>  
-              </div>  
-              {errors.query && <p className="text-red-600">{errors.query.message}</p>}  
-            </div>  
-            <div className="flex flex-col mt-[20px]">  
-              <label htmlFor="message">Message<span className=" ml-[10px] text-green-700">*</span></label>  
-              <textarea  
-                className="p-1 mt-[10px] border border-black rounded-md h-[80px] text-top align-top focus:border-2 hover:border-green-500"  
-                id="message"  
-                {...register('message')}  
-              />  
-              {errors.message && <p className="text-red-600">{errors.message.message}</p>}  
-            </div>  
-            <div className="flex items-center space-x-2 mt-[20px]">  
-              <input type="checkbox" id="agree" {...register('agree')} className="focus:border-2 hover:border-green-500" />  
-              <label htmlFor="agree" className="text-sm">I consent to being contacted by the team<span className=" ml-[10px] text-green-700">*</span></label>  
-              {errors.agree && <p className="text-red-600">{errors.agree.message}</p>}  
-            </div>  
-            <button className="mt-[40px] bg-mediumgreen w-full md:w-[600px] items-center h-[50px] rounded-md text-white hover:bg-darkergray">Submit</button>  
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-2xl bg-white p-4 rounded-2xl shadow-md">  
+        <h1 className="text-2xl font-bold font-Karla">Contact Us</h1>  
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mt-[20px]">  
+          <div className="flex flex-col flex-1">  
+            <label htmlFor="firstname">First Name<span className="ml-[10px] text-green-700">*</span></label>  
+            <input  
+              className="border cursor-pointer border-black h-[40px] rounded-[5px] p-1 mt-[10px] hover:border-2 hover:border-green-500"  
+              id="firstname"  
+              type="text"  
+              {...register('firstname')}  
+            />  
+            {errors.firstname && <p className="text-red-600">{errors.firstname.message}</p>}  
+          </div>  
+          <div className="flex flex-col flex-1">  
+            <label htmlFor="lastname">Last Name<span className="ml-[10px] text-green-700">*</span></label>  
+            <input  
+              className="border cursor-pointer border-black h-[40px] rounded-[5px] p-1 mt-[10px] hover:border-2 hover:border-green-500"  
+              id="lastname"  
+              type="text"  
+              {...register('lastname')}  
+            />  
+            {errors.lastname && <p className="text-red-600">{errors.lastname.message}</p>}  
           </div>  
         </div>  
+        <div className="flex flex-col mt-[20px]">  
+          <label htmlFor="email">Email<span className="ml-[10px] text-green-700">*</span></label>  
+          <input  
+            type="email"  
+            id="email"  
+            className="p-1 border cursor-pointer border-black rounded-[5px] w-full mt-[10px] h-[40px] hover:border-2 hover:border-green-500"  
+            {...register('email')}  
+          />  
+          {errors.email && <p className="text-red-600">{errors.email.message}</p>}  
+        </div>  
+        <div className="flex flex-col mt-[20px]">  
+          <label>Query Type<span className="ml-[10px] text-green-700">*</span></label>  
+          <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">  
+            <div className={`flex items-center border p-2 rounded-md w-full md:w-[300px] mt-[10px] ${selectedQuery === 'GeneralEnquiry' ? 'bg-lightgreen' : 'bg-white'} focus-within:border-2 focus-within:border-green-500`}>  
+              <input  
+                type="radio"  
+                id="generalenquiry"  
+                value="GeneralEnquiry"  
+                className="ml-[18px] mr-2 focus:ring-2 focus:ring-green-500"  
+                {...register('query')}  
+                onChange={() => handleRadioChange('GeneralEnquiry')}  
+              />  
+              <label htmlFor="generalenquiry">General Enquiry</label>  
+            </div>  
+            <div className={`flex items-center border p-2 rounded-md w-full md:w-[300px] mt-[10px] ${selectedQuery === 'SupportRequest' ? 'bg-lightgreen' : 'bg-white'} focus-within:border-2 focus-within:border-green-500`}>  
+              <input  
+                type="radio"  
+                id="supportrequest"  
+                value="SupportRequest"  
+                className="ml-[18px] mr-2 focus:ring-2 focus:ring-green-500"  
+                {...register('query')}  
+                onChange={() => handleRadioChange('SupportRequest')}  
+              />  
+              <label htmlFor="supportrequest">Support Request</label>  
+            </div>  
+          </div>  
+          {errors.query && <p className="text-red-600">{errors.query.message}</p>}  
+        </div>  
+        <div className="flex flex-col mt-[20px]">  
+          <label htmlFor="message">Message<span className="ml-[10px] text-green-700">*</span></label>  
+          <textarea  
+            className="p-1 mt-[10px] border border-black rounded-md h-[80px] text-top align-top focus:border-2 hover:border-green-500"  
+            id="message"  
+            {...register('message')}  
+          />  
+          {errors.message && <p className="text-red-600">{errors.message.message}</p>}  
+        </div>  
+        <div className="flex items-center space-x-2 mt-[20px]">  
+          <input type="checkbox" id="agree" {...register('agree')} className="focus:border-2 hover:border-green-500" />  
+          <label htmlFor="agree" className="text-sm">I consent to being contacted by the team<span className="ml-[10px] text-green-700">*</span></label>  
+        </div>  
+        {errors.agree && <p className="text-red-600">{errors.agree.message}</p>}  
+        <button className="mt-[40px] bg-mediumgreen w-full md:w-[600px] h-[50px] rounded-md text-white hover:bg-darkergray">Submit</button>  
       </form>  
     </div>  
   );  
 };  
 
-export default ContactForm;
+export default ContactForm;  
